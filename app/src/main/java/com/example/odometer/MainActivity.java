a8e1361391782bdeee8a3bfc779e99669f5aaee4
Almetov Kamil <almetov.kamil@gmail.com>
2021-03-07 16:01:39 +0400

    Вызов метода getDistance()

--- app/src/main/java/com/example/odometer/MainActivity.java
+++ app/src/main/java/com/example/odometer/MainActivity.java
@@ -7,7 +7,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDistance();
     }
 
     @Override
@@ -54,5 +59,22 @@ public class MainActivity extends AppCompatActivity {
             unbindService(connection);
             bound = false;
         }
+    }
+
+    private void displayDistance() {
+        final TextView distanceView = findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.;
+                if (bound && odometer != null) {
+                    distance = odometer.getDistance();
+                }
+                String distanceStr = String.format(Locale.getDefault(), "%1$,.2f meters", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
