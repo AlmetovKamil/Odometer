80ea9ee5a429e214ce6999d463992a01f8ab047b
Almetov Kamil <almetov.kamil@gmail.com>
2021-03-07 17:01:45 +0400

    LocationManager, LocationProvider, requestLocationUpdates(), checkSelfPermission()

--- app/src/main/java/com/example/odometer/OdometerService.java
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -1,12 +1,18 @@
 package com.example.odometer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+
+import androidx.core.content.ContextCompat;
 
 import java.util.Random;
 
@@ -14,6 +20,9 @@ public class OdometerService extends Service {
     private final IBinder binder = new OdometerBinder();
     private final Random random = new Random();
     private LocationListener listener;
+    private LocationManager locManager;
+    //Строка разрешения в виде константы
+    public static final String PERMISSION_STRING = android.Manifest.permission.ACCESS_FINE_LOCATION;
 
     //При создании связанной службы необходимо предоставить реализацию Binder
     public class OdometerBinder extends Binder {
@@ -44,6 +53,14 @@ public class OdometerService extends Service {
             @Override
             public void onStatusChanged(String arg0, int arg1, Bundle bundle) {}
         };
+        locManager = (LocationManager) getSystemService (Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                == PackageManager.PERMISSION_GRANTED) {
+            String provider = locManager.getBestProvider(new Criteria(), true);
+            if (provider != null) {
+                locManager.requestLocationUpdates(provider, 1000, 1, listener);
+            }
+        }
     }
 
     //Метод onBind() возвращает IBinder — интерфейс, реализуемый классом Binder
