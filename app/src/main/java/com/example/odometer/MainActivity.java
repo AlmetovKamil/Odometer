037eba8d870e0e78e4938419a09dd5c3a0bf135d
Almetov Kamil <almetov.kamil@gmail.com>
2021-03-07 15:36:27 +0400

    Создание объекта ServiceConnection

--- app/src/main/java/com/example/odometer/MainActivity.java
+++ app/src/main/java/com/example/odometer/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.odometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Создание объекта ServiceConnection
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //Код, выполняемый при связывании со службой
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            //Код, выполняемый при разрыве связи со службой
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
