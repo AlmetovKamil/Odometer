e04257897c510c764d5adeca67a580b093a37bc6
Almetov Kamil <almetov.kamil@gmail.com>
2021-03-07 11:18:05 +0400

    Определение Binder

--- app/src/main/java/com/example/odometer/OdometerService.java
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -2,15 +2,27 @@ package com.example.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class OdometerService extends Service {
+    private final IBinder binder = new OdometerBinder();
+
+    //При создании связанной службы необходимо предоставить реализацию Binder
+    public class OdometerBinder extends Binder {
+
+        //Метод используется активностью для получения ссылки на OdometerService
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
     public OdometerService() {
     }
 
+    //Метод onBind() возвращает IBinder — интерфейс, реализуемый классом Binder
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
     }
 }
